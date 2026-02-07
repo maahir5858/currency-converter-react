67be88c5a229b0a45cbeb9f4b5a66145948d0ec4
Maahir <maahirchoudhary92@gmail.com>
2026-02-07 19:48:18 +0530

    last commmit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// This file is made for Importing all the Components all-together at one place(file), and then Exporting everything from this  ((index.js))  -  & when importing this file, we dont need to write the file path -->  src/components/index.js
+
+import InputBox from "./InputBox";
+
+export { InputBox };
